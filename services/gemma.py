d9a456fef53b90f675a2621f9c536c077ebfa007
Basavaraj Patil <150652540+basavarajpatil660@users.noreply.github.com>
2026-05-27 20:01:20 +0530

    Update gemma.py

--- services/gemma.py
+++ services/gemma.py
@@ -5,6 +5,7 @@ import google.generativeai as genai
 from utils.retry import call_with_retry
 
 MODELS_IN_ORDER = [
+    "models/gemma-4-31b-it",
     "models/gemma-4-27b-it",
     "models/gemma-4-12b-it",
     "models/gemma-4-4b-it"
